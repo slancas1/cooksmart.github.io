428444befbbd1b2e91751da96049cefecb9deb8c
Brianna Hoelting <briannahoelting@briannas-mbp-3.dhcp.nd.edu>
2019-04-23 14:48:03 -0400

    execute account

--- js/createaccount.js
+++ js/createaccount.js
@@ -1,5 +1,3 @@
-
-//jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
