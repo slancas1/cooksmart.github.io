2d672de06e428fbc6d6746ea87eb2ff07ae3eeec
Brianna Hoelting <briannahoelting@briannas-mbp-3.dhcp.nd.edu>
2019-04-23 10:30:25 -0400

    some changes

--- js/createaccount.js
+++ js/createaccount.js
@@ -1,3 +1,4 @@
+
 //jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
@@ -78,4 +79,4 @@ $(".previous").click(function(){
 
 $(".submit").click(function(){
 	return false;
-});
+})
