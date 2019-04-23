5f41def6ad4955d6dd9975b45fc06568e52f9a2e
TheEmiK9 <ekoykka@LAPTOP-VUMQLE0J.localdomain>
2019-04-23 12:30:55 -0400

    Search functionality works, need formatting

---
+++ src/app.js
@@ -0,0 +1,43 @@
+/* global algoliasearch instantsearch */
+
+const searchClient = algoliasearch(
+    'J447XF3GOT',
+    '7e9319d00ee4d24a4fc6499fa50c2473'
+);
+
+const search = instantsearch({
+    indexName: 'Recipes',
+    searchClient,
+});
+
+search.addWidget(
+    instantsearch.widgets.searchBox({
+        container: '#searchbox',
+    })
+);
+
+search.addWidget(
+    instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+            item: `
+        <div max-height="400px">
+          <div class="hit-image">
+            <img src="{{imageURL}}" align="left" max-height="100px" width="auto" alt="{{name}}" />
+          </div>
+          <div class="hit-name">
+            {{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+          </div>
+        </div>
+      `,
+        },
+    })
+);
+
+search.addWidget(
+    instantsearch.widgets.pagination({
+        container: '#pagination',
+    })
+);
+
+search.start();
